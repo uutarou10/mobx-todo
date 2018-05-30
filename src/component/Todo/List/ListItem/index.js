90d301eba5365ab64afe4278d9fdcefc6cde2258
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-30 15:34:51 +0900

    Add style

--- src/component/Todo/List/ListItem/index.js
+++ src/component/Todo/List/ListItem/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
+const style = {
+  display: 'flex',
+  alignItems: 'center'
+}
+
+const checkBoxStyle = {
+  padding: '8px'
+}
+
+const titleStyle = {
+  margin: '8px 0',
+  fontWeight: 'bold'
+}
+
+const descriptionStyle = {
+  margin: '8px 0'
+}
+
 const ListItem = observer(({ todo }) => (
-  <div>
-    <div>
-      <input type="checkbox" checked={todo.isDone} onChange={() => todo.toggle()} />
+  <div style={style}>
+    <div style={checkBoxStyle}>
+      <input
+        type="checkbox"
+        checked={todo.isDone}
+        onChange={() => todo.toggle()}
+      />
     </div>
     <div>
-      <p>{ todo.title }</p>
-      <p>{ todo.description }</p>
+      <p style={titleStyle}>{ todo.title }</p>
+      <p style={descriptionStyle}>{ todo.description }</p>
     </div>
   </div>
 ));
